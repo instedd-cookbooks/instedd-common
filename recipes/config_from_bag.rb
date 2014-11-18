89178761a74fd1431e46dd4daedc06dcd2ea04c8
Juan Wajnerman <jwajnerman@manas.com.ar>
2014-11-18 17:21:54 -0300

    config_from_bag supports setting nested values

--- recipes/config_from_bag.rb
+++ recipes/config_from_bag.rb
@@ -1,6 +1,18 @@
 node['config_from_bag'].each do |bag, item|
-  data = Chef::EncryptedDataBagItem.load(bag, item)
-  data.to_hash.each do |key, value|
-    node.default[bag][key] = value
+  if item.is_a?(Hash)
+    data = Chef::EncryptedDataBagItem.load(bag, item['item'])
+    item['values'].to_hash.each do |key, path|
+      value = data[key]
+      target = node.default
+      while path.length > 1
+        target = target[path.shift]
+      end
+      target[path.shift] = value
+    end
+  else
+    data = Chef::EncryptedDataBagItem.load(bag, item)
+    data.to_hash.each do |key, value|
+      node.default[bag][key] = value
+    end
   end
 end
